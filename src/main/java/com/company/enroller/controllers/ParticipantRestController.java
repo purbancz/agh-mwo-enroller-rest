4f4b22b1d8ba21c941f5eaf4a96bc0ae74c8484c
student15 <student15@pcoip.ki.agh.edu.pl>
2019-04-14 11:41:37 +0200

    New methods and endpoints for Participants added

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,52 @@ public class ParticipantRestController {
 	public ResponseEntity<?> getParticipants() {
 		Collection<Participant> participants = participantService.getAll();
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity<Participant>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+		if (participantService.findByLogin(participant.getLogin()) != null) {
+			ResponseEntity<Participant> entity = new ResponseEntity<Participant>(HttpStatus.CONFLICT);
+			return entity;
+		} else {
+			participantService.saveParticipant(participant);
+			return new ResponseEntity("A participant with login " + participant.getLogin() + " has been created.",
+					HttpStatus.CREATED);
+		}
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteParticipant(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity("A participant with login " + login + " does not exist", HttpStatus.NOT_FOUND);
+		} else {
+			participantService.deleteParticipant(participant);
+			return new ResponseEntity("A participant with login " + login + " has been deleted.",
+					HttpStatus.NO_CONTENT);
+		}
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateParticipant(@PathVariable("id") String login,
+			@RequestBody Participant updateParticipant) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		} else {
+			participant.setPassword(updateParticipant.getPassword());
+			participantService.updateParticipant(participant);
+			return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+		}
 	}
 
 }
