5a193add318adeb04fae56c59bfd06bb0f8d299d
student15 <student15@pcoip.ki.agh.edu.pl>
2019-04-14 12:13:15 +0200

    New methods and endponts for Meetings added

---
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,20 @@
+package com.company.enroller.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.enroller.persistence.MeetingService;
+import com.company.enroller.persistence.ParticipantService;
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController {
+	
+	@Autowired
+	MeetingService meetingService;
+	
+	
+	
+
+}
